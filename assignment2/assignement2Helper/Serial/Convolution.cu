ac1a257b6b391adecf5b9902d27d18a77395b888
Shameel Nkosi <shameelnkosi@gmail.com>
2021-05-19 23:44:17 +0200

    testing

--- assignment2/assignement2Helper/Serial/Convolution.cu
+++ assignment2/assignement2Helper/Serial/Convolution.cu
@@ -5,35 +5,35 @@
 #include <helper_functions.h>
 #include <helper_cuda.h>
 
-#define DIMx 5
-#define DIMy 5
+#define maskDimx 3
 
 // input and mask are globals
 // below these are for testing purposes
-float input[DIMx][DIMy];
-float output[DIMx][DIMy];
-float mask1[3][3]; // averaging
-float mask2[3][3]; // sharpening
-float mask3[3][3]; // edging
+float mask1[maskDimx*maskDimx]; // averaging
+float mask2[maskDimx*maskDimx]; // sharpening
+float mask3[maskDimx*maskDimx]; // edging
 
 
 // l and p are the rows and colums of the mask respectively
 // i and j are the coordinates of input data point we want to mask for 
-void maskingFunc(float *inputImg , float *outputImg, int rows , int cols , int i, int j, int maskDimx,float mask[3][3]);
+void maskingFunc(float *inputImg , float *outputImg, int rows , int cols , int i, int j, float mask[maskDimx*maskDimx]);
 
 // Define the files that are to be save and the reference images for validation
 const char *imageFilename = "ref_rotated.pgm";
 const char *refFilename   = "ref_rotated.pgm";
 
 const char *sampleName = "simpleTexture";
+ //load image from disk
+ float *inputImg = NULL;
+ unsigned int width , height;
 
 
 //TESTING FUNCTIONS
 void init();
 void printOut(float *p , int y, int x);
-void printMask(float mask[3][3]);
+void printMask(float mask[maskDimx*maskDimx]);
 
-void Convolve(int argc , char **argv);
+void Convolve(int argc , char **argv, float mask[maskDimx*maskDimx]);
 
 int main( int argc, char **argv ){
     init();
@@ -41,12 +41,12 @@ int main( int argc, char **argv ){
     printMask(mask2);
     printMask(mask3);
 
-    Convolve(argc, argv );
+    Convolve(argc, argv, mask1);
     return 0;
 }
 
 // To be called seperately for each block to be masked
-void maskingFunc(float *inputImg , float *outputImg, int rows , int cols , int i, int j, int maskDimx, float mask[3][3] ){
+void maskingFunc(float *inputImg , float *outputImg, int rows , int cols , int i, int j, float mask[maskDimx*maskDimx] ){
     int L = maskDimx; // L is the mask's x-axis dimension 
     int P = maskDimx; // P is the mask's y-axis dimension
     float sum = 0.0;
@@ -57,7 +57,7 @@ void maskingFunc(float *inputImg , float *outputImg, int rows , int cols , int i
             // f is the value of the mask at given indices
             float y, f;
             y = (i-m+l) < 0 ? 0 : (j-m+p) < 0 ? 0 : (i-m+l)> (rows-1) ? 0 : (j-m+p) > (cols-1)? 0: inputImg[(i-m+l)*cols + (j-m+p)];
-            f = mask[p][l];
+            f = mask[l*maskDimx +p];
             sum += (f*y) ;
         }
     }
@@ -65,41 +65,67 @@ void maskingFunc(float *inputImg , float *outputImg, int rows , int cols , int i
     outputImg[ i*cols + j] = sum ;
 }
 
-void Convolve(int argc , char **argv){
+/*
+__global__ void globalConvolve(float *inIMG, float *outIMG, float *mask, float width, float height, float DIMx){
+    // the 
 
-    //load image from disk
-    float *hData = NULL;
-    unsigned int width , height;
+    int i = threadIdx.x + blockIdx.x*blockDim.x;
+    int j = threadIdx.y + blockIdx.y*blockDim.y;
 
-    // int q = 512*511;
+
+    int L = DIMx; // L is the mask's x-axis dimension 
+    int P = DIMx; // P is the mask's y-axis dimension
+    float sum = 0.0;
+    int m = (DIMx - 1)/2; // This handles different size mask dimensions i.e 3, 5, 7 etc
+    for(int l = 0; l < L; l++){
+        for(int p = 0; p < P; p++){
+            // y is the value in input
+            // f is the value of the mask at given indices
+            float y, f;
+            y = (i-m+l) < 0 ? 0 : (j-m+p) < 0 ? 0 : (i-m+l)> (height-1) ? 0 : (j-m+p) > (width-1)? 0: inIMG[(i-m+l)*width + (j-m+p)];
+            f = mask[l*DIMx + p];
+            sum += (f*y) ;
+        }
+    }
+    outIMG[ i*width + j] = sum ;
+}
+*/
+void Convolve(int argc, char **argv, float mask[maskDimx*maskDimx]){
+
+   
+    // Get the image path for for the image file name
     char* imagePath = sdkFindFilePath(imageFilename, argv[0]);
     
     if(imagePath == NULL){
         printf("Unable to source image file: %s\n",imageFilename);
         exit(EXIT_FAILURE);
     }
-
-    sdkLoadPGM(imagePath, &hData, &width, &height);
+    // Load the image here
+    sdkLoadPGM(imagePath, &inputImg, &width, &height);
     unsigned int size = width * height * sizeof(float);
-    printf("Loaded '%s', %d x %d pixels\nand size is %d\n",imageFilename, width, height,size);
-    // printf("%f\n",hData[q]);
-    // printOut(hData, width, height);
 
     float *outputImg = (float *) malloc(size);
 
     for(int k = 0; k < width * height; k++){
         int i = k/width;
         int j = k - (i*width);
-        maskingFunc(hData, outputImg, width , height, i , j , 3, mask2);
+        maskingFunc(inputImg, outputImg, width , height, i , j , mask);
     }
-    // printf("%f\n",outputImg[q]);
-
-    // outputfileName
+ 
     char outputFilename[1024];
     strcpy(outputFilename, imagePath);
     strcpy(outputFilename + strlen(imagePath) - 4, "_out.pgm");
     sdkSavePGM(outputFilename, outputImg, width, height);
     printf("Wrote '%s'\n", outputFilename);
+
+    // float *gInputImg , *gOutputImg, *gMask;
+
+    
+    // HANDLE_ERROR(cudaMalloc((void**)&gInputImg , size));
+    // HANDLE_ERROR(cudaMalloc((void**)&gOutputImg , size));
+    // HANDLE_ERROR(cudaMalloc((void**)&gMask, maskDimx*maskDimx*sizeof(float)));
+
+
 }
 
 
@@ -109,14 +135,14 @@ void init(){
     // for the mask
 
     // for the input
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j< 3; j++){
-            mask1[i][j] = (float) 1/9; // average
-            mask2[i][j] = -1; // sharpening
-            mask3[i][j] = (j == 0 && i != 1)?-1:(j == 0 && i == 1)? -2:(j == 2 && i != 1)?1:(j == 2 && i == 1)? 2:0; // edge
+    for(int i = 0; i < maskDimx; i++){
+        for(int j = 0; j< maskDimx; j++){
+            mask1[i*maskDimx + j] = (float) 1/(maskDimx*maskDimx); // average
+            mask2[i*maskDimx + j] = -1; // sharpening
+            mask3[i*maskDimx + j] = (j == 0 && i != 1)?-1:(j == 0 && i == 1)? -2:(j == 2 && i != 1)?1:(j == 2 && i == 1)? 2.0:0.0; // edge
         }
     }
-    mask2[1][1] = 8;
+    mask2[(maskDimx/2)*maskDimx + maskDimx/2] = 9;
     
 
 }
@@ -137,10 +163,10 @@ void printOut(float *p , int width, int height){
 }
 
 // Prints a mask
-void printMask(float mask[3][3]){
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            printf("%f ",mask[i][j]);
+void printMask(float mask[maskDimx*maskDimx]){
+    for(int i = 0; i < maskDimx; i++){
+        for(int j = 0; j < maskDimx; j++){
+            printf("%f ",mask[i*maskDimx + j]);
         }
         printf("\n");
     }
