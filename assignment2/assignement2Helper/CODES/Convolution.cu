ace8201185ba2e7acb42b640f895c1ffdbbbcdfd
Shameel Nkosi <shameelnkosi@gmail.com>
2021-05-23 11:52:06 +0200

    added the sharp

--- assignment2/assignement2Helper/CODES/Convolution.cu
+++ assignment2/assignement2Helper/CODES/Convolution.cu
@@ -9,7 +9,7 @@
 
 #define maskDimx 3
 #define tileWidth 8 // this will be used for the shared memory code
-#define maskChoice 1
+#define maskChoice 2
 
 // input and mask are globals for the serial code
 float mask1[maskDimx*maskDimx]; // averaging
@@ -22,7 +22,7 @@ float mask3[maskDimx*maskDimx]; // edging
 void maskingFunc(float *inputImg , float *outputImg, int rows , int cols , int i, int j, float mask[maskDimx*maskDimx]);
 
 // Define the files that are to be save and the reference images for validation
-const char *imageFilename = "mandrill/averaging/mandrill.pgm";
+const char *imageFilename = "image21/sharp/image21.pgm";
 
  //load image from disk
  float *inputImg = NULL;
@@ -67,7 +67,7 @@ __global__ void globalConvolve(float *inIMG, float *outIMG, int *gParams){
                 // f is the value of the mask at given indices
                 float y, f;
                 y = (i-m+l) < 0 ? 0 : (j-m+p) < 0 ? 0 : (i-m+l)> (height-1) ? 0 : (j-m+p) > (width-1)? 0: inIMG[(i-m+l)*width + (j-m+p)];
-                f = ave[l*DIMx + p];
+                f = sharp[l*DIMx + p];
                 sum += (f*y) ;
             }
         }
@@ -94,7 +94,7 @@ __global__ void sharedConvolve(float *inputImg, float *outputImg, int *gParams){
     for(int p=0;p<DIMx;++p){
         for(int l=0;l<DIMx;++l){
 
-            float MASK = ave[p*DIMx+l];
+            float MASK = sharp[p*DIMx+l];
 
             if((i-m+p)<0){
                 sum = sum + 0; 
@@ -121,7 +121,7 @@ __global__ void sharedConvolve(float *inputImg, float *outputImg, int *gParams){
                 sum = sum + inputImg[(i-m+p)*width+(j-m+l)]*MASK;
             }
             else{
-                sum = sum + sharedMem[(threadIdx.x-m+l)][(threadIdx.y-m+p)]*ave[p*DIMx+l];
+                sum = sum + sharedMem[(threadIdx.x-m+l)][(threadIdx.y-m+p)]*MASK;
             }
         }
     }
@@ -163,7 +163,7 @@ __global__ void texConvolve(float *outputImg,int *gParams){
                 sum = sum + 0;
             }
             else{
-                sum += tex2D(tex,j-m+l , i-m+p)*tex2D(tex_av,l,p);
+                sum += tex2D(tex,j-m+l , i-m+p)*tex2D(tex_sharp,l,p);
             }
         }
     }
